185fb5df20a292dcd692fb3400e44e349a3b855f
Rachit Singh <rachit.singh_cs19@gla.ac.in>
2020-12-06 23:28:58 +0530

    Registration Screen updated

--- lib/Screen/registration_screen.dart
+++ lib/Screen/registration_screen.dart
@@ -1,3 +1,7 @@
+import 'package:country_pickers/country.dart';
+import 'package:country_pickers/country_picker_dialog.dart';
+import 'package:country_pickers/utils/utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -6,8 +10,101 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  Country _selectedFilteredDialogCountry =
+      CountryPickerUtils.getCountryByPhoneCode("91");
+  String _countryCode = "+91";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text("              "),
+                  Text(
+                    "Verify your phone Number",
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
+                  ),
+                  Text("        "),
+                  Icon(Icons.more_vert),
+                ],
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                "Whatsapp will send an SMS message to verify your phone number. Enter your country Code and phone number",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              ListTile(
+                onTap: _openFilteredCountryPickerDialog,
+                title: _buildDialogItem(_selectedFilteredDialogCountry),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 14.0),
+                child: Row(
+                  children: [Container()],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _openFilteredCountryPickerDialog() {
+    showDialog(
+      context: context,
+      builder: (_) => Theme(
+        data: Theme.of(context).copyWith(
+          primaryColor: Colors.white,
+        ),
+        child: CountryPickerDialog(
+          titlePadding: EdgeInsets.all(8.0),
+          searchCursorColor: Colors.black,
+          searchInputDecoration: InputDecoration(
+            hintText: "Search",
+          ),
+          isSearchable: true,
+          title: Text("Select your phone code"),
+          onValuePicked: (Country country) {
+            _selectedFilteredDialogCountry = country;
+            _countryCode = country.phoneCode;
+          },
+          itemBuilder: _buildDialogItem,
+        ),
+      ),
+    );
+  }
+
+  _buildDialogItem(Country country) {
+    return Container(
+        height: 40.0,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            border:
+                Border(bottom: BorderSide(color: Colors.green, width: 1.0))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CountryPickerUtils.getDefaultFlagImage(country),
+            SizedBox(
+              width: 10.0,
+            ),
+            Text("+${country.phoneCode}", style: TextStyle(fontSize: 13.0)),
+            SizedBox(width: 4.0),
+            Text("${country.name}", style: TextStyle(fontSize: 13.0)),
+            Spacer(),
+            Icon(Icons.arrow_drop_down),
+          ],
+        ));
   }
 }
