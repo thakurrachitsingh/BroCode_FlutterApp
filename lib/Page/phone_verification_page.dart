d6fb33476a5d3186d2b5d55f280255d05c0f2d63
Rachit Singh <rachit.singh_cs19@gla.ac.in>
2020-12-08 00:15:04 +0530

    Phone Verification created

--- lib/Page/phone_verification_page.dart
+++ lib/Page/phone_verification_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pin_entry_text_field/pin_entry_text_field.dart';
+import 'set_profile_page.dart';
 
 class PhoneVerificationPage extends StatefulWidget {
   @override
@@ -8,6 +10,87 @@ class PhoneVerificationPage extends StatefulWidget {
 class _PhoneVerificationPageState extends State<PhoneVerificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(""),
+                Text(
+                  "Verify your phone number",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.green,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Icon(Icons.more_vert)
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Whatsapp will send an SMS message to verify your phone number. Enter your country Code and phone number",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            _pincodeWidget(),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: MaterialButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => SetProfile(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Next",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pincodeWidget() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: <Widget>[
+          PinEntryTextField(
+            onSubmit: (String pin) {
+              print(pin);
+            }, // end onSubmit
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Enter your 4 digit code")
+        ],
+      ),
+    );
   }
 }
